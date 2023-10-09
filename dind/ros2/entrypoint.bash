ba7b41f1239315ed43d71834e778adc2cada2b06
Taiga Takano <ttttghghnb554z@outlook.jp>
2023-10-09 14:02:02 +0900

    Push!!!
    Signed-off-by: Taiga Takano <ttttghghnb554z@outlook.jp>

---
+++ dind/ros2/entrypoint.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo ''
+echo '======== Loading the ROS 2 environment ========'
+echo ''
+
+CMD="source /opt/ros/${ROS2_DISTRO}/setup.sh"
+echo "$CMD" && eval "$CMD" || exit $?
+
+echo ''
+echo '======== Building the workspace ========'
+echo ''
+
+cd /ws && colcon build \
+  --event-handlers console_cohesion+ \
+  --cmake-args || exit $?
+
+source install/setup.bash || exit $?
